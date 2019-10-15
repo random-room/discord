097085ce76251946d0cab40317c0634f6799329b
random-room <56581436+random-room@users.noreply.github.com>
2019-10-15 19:20:12 +1100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.NjI3MDkwOTI2Nzc0Mzg2NzEw.XaV-xQ.0Fv8SVPp7ktDDETP8a1CHLCFSyU);
